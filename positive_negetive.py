a1a30dc2124dcf862b49643d5f6eb18fbad87cc4
Gagan-DN <116274483+Gagan-DN@users.noreply.github.com>
2024-01-06 23:06:15 +0530

    Create positive_negetive.py

---
+++ positive_negetive.py
@@ -0,0 +1,8 @@
+
+#7 cheak the number is positive or negetive
+if nterms<=0:
+        print("please enter a positive number")
+else:
+    print("fabonacci Sequence:")
+    for i in range(nterms):
+         print(fabi(i))
